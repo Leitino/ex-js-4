9f18cbd914ddbec41938e36bd6902dceffe2d9ca
Leitino <alessandro.leita89@gmail.com>
2018-03-19 18:01:15 +0100

    finito, senza parte bonus

--- es.js
+++ es.js
@@ -1,19 +1,23 @@
 var numCasuale_lista=[]; //creato lista vuota
 var tentativo = prompt('inserisci numero') //utente inserisce
+var tentativi_riusciti=[]
 
-for (var i = 0; i < 16; i++) {
+for (var i = 0; i < 16; i++) {//ciclo for che si ripete 16 volte e crea numeri casuali da 1 a 100
   numCasuale = Math.floor((Math.random()*100)+1);
-  numCasuale_lista.push(numCasuale)
+  numCasuale_lista.push(numCasuale)//inserisco il numero casuale nella lista
 }
   console.log(tentativo);
   console.log(numCasuale_lista);
 
-for (var i = 0; i < 100; i++) {
+
+contatore=0
+while ((tentativo!=numCasuale_lista)&&(contatore<100)) {
   tentativo = prompt('inserisci numero')
   tentativo = parseInt(tentativo)
+  tentativi_riusciti.push(tentativo)
+  contatore++
   if (numCasuale_lista.includes(tentativo)) {
-    alert('hai perso')
-
+    alert('hai perso, hai totalizzato ' + tentativi_riusciti.length + ' punti')
+    break;
   }
-
 }
